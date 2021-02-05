0dba4974133394ec42437237a238bfcb546992b9
Amar Ramdas <amarramdas@asrr.nl>
2021-02-05 10:26:52 +0100

    Implement tests and gradle additions for jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,24 @@
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+repositories {
+	maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+}
 
 plugins {
 	id("org.springframework.boot") version "2.5.0-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.21-2"
 	kotlin("plugin.spring") version "1.4.21-2"
+//	jacoco
+	jacoco
+
 }
+
 
 group = "nl.asrr"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_15
+//java.sourceCompatibility = JavaVersion.VERSION_15
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -27,13 +36,15 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "15"
+		jvmTarget = "11"
 	}
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
