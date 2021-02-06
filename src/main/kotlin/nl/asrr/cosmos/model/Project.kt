5fb720dae9b3d987055762a90b4cc6b98a443a9f
Amar Ramdas <amar.ramdas@asrr.nl>
2021-02-06 19:11:12 +0100

    Create projectRepository

---
+++ src/main/kotlin/nl/asrr/cosmos/model/Project.kt
@@ -0,0 +1,7 @@
+package nl.asrr.cosmos.model
+
+data class Project(val name: String,
+                   val client: String,
+                   val code: String,
+                   val budget: Int?
+)
