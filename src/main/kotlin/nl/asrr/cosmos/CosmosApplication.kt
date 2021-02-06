513f30e67e490dc866219ede9428c0cd9ae29d9d
Amar Ramdas <amar.ramdas@asrr.nl>
2021-02-07 00:49:54 +0100

    Add some swagger documentation tags to projectcontroller

--- src/main/kotlin/nl/asrr/cosmos/CosmosApplication.kt
+++ src/main/kotlin/nl/asrr/cosmos/CosmosApplication.kt
@@ -2,12 +2,10 @@ package nl.asrr.cosmos
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import javax.annotation.processing.Generated
 
 @SpringBootApplication
 class CosmosApplication
 
-@Generated
 fun main(args: Array<String>) {
     runApplication<CosmosApplication>(*args)
 }
--- src/main/kotlin/nl/asrr/cosmos/controller/ProjectController.kt
+++ src/main/kotlin/nl/asrr/cosmos/controller/ProjectController.kt
@@ -1,5 +1,6 @@
 package nl.asrr.cosmos.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import nl.asrr.cosmos.dto.ProjectCreationDto
 import nl.asrr.cosmos.model.Project
 import nl.asrr.cosmos.repository.ProjectRepository
@@ -16,18 +17,21 @@ class ProjectController(
 ) {
 
     @GetMapping
+    @Operation(summary = "Retrieve all projects", description = "This call retrieves all projects without a filter.")
     fun getAllProjects(): ResponseEntity<List<Project>> {
         val projects = projectRepository.findAll()
         return ResponseEntity.ok(projects)
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retrieve projects by id", description = "This call retrieves a project by id.")
     fun getOneProject(@PathVariable("id") id: String): ResponseEntity<Project> {
         val project = projectRepository.findOneById(id)
         return ResponseEntity.ok(project)
     }
 
     @PostMapping
+    @Operation(summary = "Create new project")
     fun createProject(@RequestBody projectCreationDto: ProjectCreationDto): ResponseEntity<Project> {
         return projectService.createProject(projectCreationDto);
     }
